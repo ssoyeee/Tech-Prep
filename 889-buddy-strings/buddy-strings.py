26129dc58a9b6269cfdb82b018e0435b85033bfb
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2024-09-25 20:21:46 -0400

    Time: 46 ms (10.01%) | Memory: 16.8 MB (16.38%) - LeetSync

--- 889-buddy-strings/buddy-strings.py
+++ 889-buddy-strings/buddy-strings.py
@@ -5,26 +5,29 @@ class Solution:
         if len(s) != len(goal):
            return False
 
-        # create the Counter object to see if the number of the characters
-        #We can also use .keys() and .values() methods to access Counter class object
-        s_char, goal_char = Counter(s), Counter(goal) #O(1), dict only have 26 letters
-        if s_char != goal_char: #maybe different character, different length
+        # create the Counter object to see if the number of the characters (iterable객체를 돌면서 각 요소가 몇번 등장하는지 체크)
+        # We can also use .keys() and .values() methods to access Counter class object
+        s_char, goal_char = Counter(s), Counter(goal) # O(1), dict only have 26 letters
+        if s_char != goal_char: # maybe different character, different length
             return False
         
         diff = 0
         for i in range(len(s)):
-            if s[i] != goal[i]:
+            if s[i] != goal[i]: #diff를 만드는 법, 실제로 아예 같은지 비교
                 diff += 1
 
-        if diff == 2:
-            return True
+        if diff == 2: #위에 방식대로 비교를해서 2가 된다면
+            return True #ab ba
 
         elif diff == 0:
-            if max(s_char.values())> 1: 
+            if max(s_char.values())> 1: #s의 counter에서 values의 max가 at least 2라면 always true
                 return True 
             else:
                 return False
-        else:
+        else: #1,3이면 false
             return False
+
+    #time O(max(s, goal))
+    #space O(1)
 
 
