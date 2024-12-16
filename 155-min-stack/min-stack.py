5a581b169d0fc226b16d92649eadef039954ef64
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2024-12-16 11:24:15 -0500

    Time: 5 ms (62.37%) | Memory: 21.1 MB (11.32%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -1,6 +1,27 @@
 class MinStack:
-
+    # Design a stack which has 4 operations - push, pop, top, getMin
+    # Implement O(1) time,  O(n) space, 1 stack
     def __init__(self):
+        self.stack = []
+    
+    def push(self, val):
+        # Determine the new minimum value
+        if not self.stack:
+            current_min = val # if stack is empty, new element = new min
+        else:
+            current_min = min(val, self.stack[-1][1])
+        self.stack.append((val, current_min))
+    
+    def pop(self):
+        self.stack.pop()
+    
+    def top(self):
+        return self.stack[-1][0]
+    
+    def getMin(self):
+        return self.stack[-1][1]
+
+    '''def __init__(self):
         self.stack = []
         self.minStack = [] #min(second) stack
 
@@ -26,4 +47,4 @@ class MinStack:
 # obj.push(val)
 # obj.pop()
 # param_3 = obj.top()
-# param_4 = obj.getMin()
+# param_4 = obj.getMin()'''
