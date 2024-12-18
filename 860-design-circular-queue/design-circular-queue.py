2a39bf5fce552e21aba4426d093bc08a3d954524
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2024-12-18 00:42:18 -0500

    Time: 11 ms (31.94%) | Memory: 18.4 MB (5.52%) - LeetSync

--- 860-design-circular-queue/design-circular-queue.py
+++ 860-design-circular-queue/design-circular-queue.py
@@ -1,39 +1,39 @@
 class MyCircularQueue:
     # ring buffer
     def __init__(self, k: int):
-        self.k = [None] * k
+        self.q = [None] * k
         self.maxlen = k
         self.front = 0
         self.rear = 0
     # enQueue(): move rear pointer
     def enQueue(self, value: int) -> bool:
-        if self.k[self.rear] is None:
-            self.k[self.rear] = value
+        if self.q[self.rear] is None:
+            self.q[self.rear] = value
             self.rear = (self.rear + 1) % self.maxlen
             return True
         else:
             return False
     # deQueue(): move front pointer
     def deQueue(self) -> bool:
-        if self.k[self.front] is None:
+        if self.q[self.front] is None:
             return False
         else:
-            self.k[self.front] = None
+            self.q[self.front] = None
             self.front = (self.front + 1) % self.maxlen 
             # %: 포인터의 위치가 전체 길이를 벗어나지 않도록
             return True      
 
     def Front(self) -> int:
-        return -1 if self.k[self.front] is None else self.k[self.front]
+        return -1 if self.q[self.front] is None else self.q[self.front]
 
     def Rear(self) -> int:
-        return -1 if self.k[self.rear - 1] is None else self.k[self.rear - 1]
+        return -1 if self.q[self.rear - 1] is None else self.q[self.rear - 1]
 
     def isEmpty(self) -> bool:
-        return self.front == self.rear and self.k[self.front] is None
+        return self.front == self.rear and self.q[self.front] is None
     # rear와 front가 만나게 되면 full
     def isFull(self) -> bool:
-        return self.front == self.rear and self.k[self.front] is not None
+        return self.front == self.rear and self.q[self.front] is not None
 
 # Your MyCircularQueue object will be instantiated and called as such:
 # obj = MyCircularQueue(k)
