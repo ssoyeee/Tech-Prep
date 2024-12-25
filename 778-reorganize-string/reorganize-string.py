ee549954d774c5f1797744b07e434f7b98f06cb5
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2024-12-25 00:33:00 -0500

    Time: 3 ms (52.66%) | Memory: 17.9 MB (6.88%) - LeetSync

--- 778-reorganize-string/reorganize-string.py
+++ 778-reorganize-string/reorganize-string.py
@@ -3,9 +3,9 @@ from collections import Counter
 
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        word2count = Counter(s)
+        char2count = Counter(s)
         heap = []
-        for char, count in word2count.items():
+        for char, count in char2count.items():
             heap.append([-count, char])
         heapify(heap) # avg: O(n)
         answer = ''
@@ -24,27 +24,3 @@ class Solution:
                 prev = [count, char]
 
         return answer
-
-        '''
-        count = Counter(s) 
-        maxHeap = [[-cnt, char] for char, cnt in count.items()]
-        heapq.heapify(maxHeap)  
-
-        prev = None
-        res = ""
-        while maxHeap or prev:
-            if prev and not maxHeap:
-                return ""
-            # most frequent, except prev
-            cnt, char = heapq.heappop(maxHeap)
-            res += char
-            cnt += 1
-
-            if prev:
-                heapq.heappush(maxHeap, prev)
-                prev = None
-            if cnt != 0:
-                prev = [cnt, char]
-        return res
-'''
-        
