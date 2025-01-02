1d28579b6657f8daeaba6c30941fc2614b65b284
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2025-01-02 15:03:02 -0500

    Time: 9 ms (19.62%) | Memory: 19.2 MB (21.41%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
@@ -1,5 +1,38 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        # union find
+        parent = [i for i in range(n)]
+        rank = [1] * n
+
+        def find(n1):
+            result = n1
+
+            while result != parent[result]:
+                parent[result] = parent[parent[result]]
+                result = parent[result]
+            return result
+        
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+
+            if p1 == p2:
+                return 0
+            
+            if rank[p2]> rank[p1]:
+                parent[p1] = p2
+                rank[p2] += rank[p1]
+            else:
+                parent[p2] = p1
+                rank[p1] += rank[p2]
+            return 1
+
+        result = n
+        for n1, n2 in edges:
+            result -= union(n1, n2)
+        return result
+
+        '''
+        # dfs
         def dfs(index):
             visited[index] = True
             for i in range(n):
@@ -23,3 +56,6 @@ class Solution:
                 answer += 1  
 
         return answer
+        T: O(E+V)
+        S: O(E+V)
+        '''
