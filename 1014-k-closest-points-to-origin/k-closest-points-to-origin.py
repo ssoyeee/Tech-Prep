db1a78333d8d4185c2616ef33374ace4ca73c6b7
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2025-01-21 13:22:51 -0500

    Time: 67 ms (62.16%) | Memory: 22.2 MB (61.94%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -3,8 +3,10 @@ class Solution:
         max_heap = []  #smallest-closest
         for point in points:
             distance = -(point[0] ** 2 + point[1] ** 2)  # negative because of maxheap
-            heapq.heappush(max_heap, (distance, point)) 
+            heapq.heappush(max_heap, (distance, point)) # in Python, heappush - sorted by 1st element in tuple
             if len(max_heap) > k:
                 heapq.heappop(max_heap)  
 
         return [item[1] for item in max_heap]  #return point
+        # Time: O(N*log K) where N is the number of list points , K is size of heap
+        # Space: O(K) where k is max heap size
