1c749de8f264793c8bb574e543f69bdae8720b78
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2025-02-05 23:58:33 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (39.94%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -4,10 +4,14 @@ class Solution:
         rotten = {(i, j) for i in range(row) for j in range(col) if grid[i][j] == 2}
         fresh = {(i, j) for i in range(row) for j in range(col) if grid[i][j] == 1}
         timer = 0
-        
+        new_dir = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         while fresh:
-            if not rotten: return -1
-            rotten = {(i+di, j+dj) for i, j in rotten for di, dj in [(0, 1), (1, 0), (0, -1), (-1, 0)] if (i+di, j+dj) in fresh}
+            if not rotten: 
+                return -1
+            rotten = {(i+di, j+dj) for i, j in rotten for di, dj in new_dir if (i+di, j+dj) in fresh}
             fresh -= rotten
             timer += 1
         return timer
+
+        # Time O(m*n) where m, n are the number of rows and col
+        # Space O(m*n) in worst case, when all org are fresh
