4b835b6c50da343d1b9ee82aef29704629efc66f
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2025-02-06 19:34:32 -0500

    Time: 1 ms (90.49%) | Memory: 17.8 MB (64.19%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,5 +1,32 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        fresh = 0
+        queue = deque()
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    fresh += 1
+                elif grid[i][j] == 2:
+                    queue.append((i, j))      
+        minutes = 0
+        directions = [(-1,0),(0,-1),(1,0),(0,1)] #u, l, d, r
+
+        while queue and fresh > 0:
+            size = len(queue)
+            for _ in range(size):
+                row, col = queue.popleft()
+
+                for dx, dy in directions:
+                    newRow, newCol = row + dx, col + dy
+                    if 0 <= newRow < m and 0 <= newCol < n and grid[newRow][newCol] == 1:
+                        queue.append((newRow, newCol))
+                        fresh -= 1
+                        grid[newRow][newCol] = 2
+            minutes += 1
+        return minutes if fresh == 0 else -1  
+        '''
         row, col = len(grid), len(grid[0])
         rotten = {(i, j) for i in range(row) for j in range(col) if grid[i][j] == 2}
         fresh = {(i, j) for i in range(row) for j in range(col) if grid[i][j] == 1}
@@ -12,6 +39,7 @@ class Solution:
             fresh -= rotten
             timer += 1
         return timer
+        '''
 
         # Time O(m*n) where m, n are the number of rows and col
         # Space O(m*n) in worst case, when all org are fresh
