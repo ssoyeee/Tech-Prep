c5cf7af9433acfe09ef38e884c69b32df7b104c9
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2025-02-10 23:49:45 -0500

    Time: 4856 ms (34.60%) | Memory: 18.2 MB (20.00%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,23 +1,29 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        if not board:
-            return False
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if self.dfs(board, i, j, word):
-                    return True
-        return False
-
-
-    def dfs(self, board, i, j, word):
-        if len(word) == 0: 
-            return True
-        if i<0 or i>=len(board) or j<0 or j>=len(board[0]) or word[0]!=board[i][j]:
-            return False
-        tmp = board[i][j]  
-        board[i][j] = "#" 
+        m = len(board)
+        n = len(board[0])
+        visited_set  = set()
+        res = False
         
-        res = self.dfs(board, i+1, j, word[1:]) or self.dfs(board, i-1, j, word[1:]) \
-        or self.dfs(board, i, j+1, word[1:]) or self.dfs(board, i, j-1, word[1:])
-        board[i][j] = tmp
-        return res
+        def dfs(i,j,visited_set,word,index):
+            if index == len(word):
+                return True
+            if i<0 or i>m-1 or j <0 or j>n-1 or (i,j) in visited_set or word[index] != board[i][j]:
+                return False
+            visited_set.add((i,j))
+
+            res = (dfs(i+1,j,visited_set,word,index+1) or 
+            dfs(i,j+1,visited_set,word,index+1) or
+            dfs(i-1,j,visited_set,word,index+1) or
+            dfs(i,j-1,visited_set,word,index+1))
+
+            visited_set.remove((i,j))
+            return res
+        
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == word[0]:
+                    if dfs(i,j,visited_set,word,0):
+                        return True
+                        
+        return False
