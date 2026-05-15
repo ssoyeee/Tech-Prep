3c553c4e1ef86214ea5855d716e4056cec8a2338
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-05-15 17:34:39 -0400

    Time: 0 ms (100.00%) | Memory: 20 MB (41.79%) - LeetSync

---
+++ 1609-find-all-the-lonely-nodes/find-all-the-lonely-nodes.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getLonelyNodes(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        
+        def dfs(node):
+            if node is None:
+                return
+            if node.left is None and node.right is not None:
+                result.append(node.right.val)
+            if node.right is None and node.left is not None:
+                result.append(node.left.val)
+            dfs(node.left)
+            dfs(node.right)
+        
+        dfs(root)
+        return result
+    
+    # closer pattern
+    # T: O(N) -- visit every node
+    # S: O(N) -- result array + recursion call stack
