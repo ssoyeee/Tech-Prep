f015ce80f82eb57b61c1a051725d2da289f2dc27
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-05-21 11:33:30 -0400

    Time: 0 ms (100.00%) | Memory: 12.4 MB (63.78%) - LeetSync

--- 671-second-minimum-node-in-a-binary-tree/second-minimum-node-in-a-binary-tree.py
+++ 671-second-minimum-node-in-a-binary-tree/second-minimum-node-in-a-binary-tree.py
@@ -13,13 +13,17 @@ class Solution(object):
         def dfs(node, min_val):
             if node is None:
                 return -1
+            # found val larger than root.val -> 2nd min
             if node.val > min_val:
                 return node.val
+            # same as root val, keep going down
             left = dfs(node.left, min_val)
             right = dfs(node.right, min_val)
 
+            # if one side has no candid, return the other
             if left == -1: return right
             if right == -1: return left
+            # both sides have candid, return smaller one
             return min(left, right)
-
+        # root.val is 1st min
         return dfs(root, root.val)
