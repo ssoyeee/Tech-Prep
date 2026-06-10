bf952b8edeedcf5a9558c98cc922e6e16dfcf632
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-06-10 03:05:52 -0400

    Time: 3 ms (74.71%) | Memory: 12.5 MB (69.24%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,23 +1,18 @@
-class Solution:
-    def isValid(self, s: str) -> bool:
-        stack = [] #store opening bracket
-        brackets = {"(":")",
-                    "[":"]",
-                    "{":"}"} 
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        bracket = {')':'(',
+                    '}':'{',
+                    ']':'['}
         for char in s:
-            if char in brackets: #if current char is one of 'keys' in brackets dictionary -> open bracket
-        #   if char in brackets.keys():     
-                stack.append(char)#push into the stack
-            elif (not stack) or (brackets[stack.pop()] != char): 
-                #if stack is not valid or brackets from stack and current char are not matched
-                return False
-        return len(stack)==0 #if stack is empty
-    #idea
-    '''
-    1) use stack
-    2) use dict
-    3) check stack is empty
-    ''' 
-    
-    #Time: O(n)
-    #Space: O(n)
+            if char in bracket: #if closed
+                if not stack or stack[-1] != bracket[char]:
+                    return False
+                stack.pop()
+            else: #if opened
+                stack.append(char)
+        return not stack
