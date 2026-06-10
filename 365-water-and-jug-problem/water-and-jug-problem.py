6f7541eb2016cbdeaa16453a23522e842d946591
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-06-10 02:41:53 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (81.03%) - LeetSync

--- 365-water-and-jug-problem/water-and-jug-problem.py
+++ 365-water-and-jug-problem/water-and-jug-problem.py
@@ -5,3 +5,8 @@ class Solution:
         if target > x+y :
             return False
         return target % gcd(x,y) == 0 #if target is divisible by gcd
+
+        #Bezout's theorem: target must be a multiple of gcd(x, y)
+
+        # T: O(logN) -- gcd calculation
+        # S: O(1)
