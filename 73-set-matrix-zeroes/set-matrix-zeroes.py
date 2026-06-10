2cb64374106d17a62039f7643636ede9003c52e1
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-06-10 02:22:46 -0400

    Time: 13 ms (46.30%) | Memory: 13.3 MB (39.54%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,20 +1,51 @@
 class Solution(object):
-    def setZeroes(self, matrix: List[List[int]]) -> None:
-        rows = set()
-        cols = set()
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        rows = len(matrix)
+        cols = len(matrix[0])
 
-        for r in range(len(matrix)):
-            for c in range(len(matrix[0])):
+        first_row_has_zero = False
+        first_col_has_zero = False
+
+        #check if the first row has zero
+        for c in range(cols):
+            if matrix[0][c] == 0:
+                first_row_has_zero = True
+                break
+
+        # check if the first column has zero
+        for r in range(rows):
+            if matrix[r][0] == 0:
+                first_col_has_zero = True
+                break
+        # use the 1st row and column as a note
+        for r in range(1, rows):
+            for c in range(1, cols):
                 if matrix[r][c] == 0:
-                    rows.add(r)
-                    cols.add(c)
+                    matrix[r][0] = 0
+                    matrix[0][c] = 0
         
-        for r in rows:
-            for c in range(len(matrix[0])):
-                matrix[r][c] = 0
-        for c in cols:
-            for r in range(len(matrix)):
-                matrix[r][c]=0
+        # set the marked rows to zero
+        for r in range(1, rows):
+            if matrix[r][0] == 0: # if 1st row marked 
+                for c in range(1, cols):
+                    matrix[r][c] = 0
+        # set the marked columns to zero
+        for c in range(1, cols):
+            if matrix[0][c] == 0: # if 1st col marked
+                for r in range(1, rows):
+                    matrix[r][c] = 0
+
+        # set the first row to zero if needed
+        if first_row_has_zero:
+            for c in range(cols):
+                matrix[0][c] = 0
         
-        # T: O(M*N) -- visit all cells in matrix
-        # S: O(M+N) -- 2 sets storing row and col indices
+        if first_col_has_zero:
+            for r in range(rows):
+                matrix[r][0] = 0
+        
+        return matrix
