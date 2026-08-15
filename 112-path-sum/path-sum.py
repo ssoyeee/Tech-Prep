cca472e69a24c46ff89e647972d697faec4fcdf7
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-08-14 21:57:39 -0400

    Time: 3 ms (44.50%) | Memory: 14.1 MB (66.24%) - LeetSync

--- 112-path-sum/path-sum.py
+++ 112-path-sum/path-sum.py
@@ -14,9 +14,14 @@ class Solution(object):
         def has_sum(root, cur_sum):
             if not root:
                 return False
+
             cur_sum += root.val
 
             if not root.left and not root.right:
                 return cur_sum == targetSum
+
             return has_sum(root.left, cur_sum) or has_sum(root.right, cur_sum)
         return has_sum(root, 0)
+
+        # T: O(N) where N is the number of nodes in tree
+        # S: O(H) in worst case O(N)
