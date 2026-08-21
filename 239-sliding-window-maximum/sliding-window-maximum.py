e7c0bab81c50a19056e1dffc457e82660138a6e0
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-08-20 23:40:52 -0400

    Time: 166 ms (91.03%) | Memory: 35.3 MB (46.27%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,19 +1,24 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        dq = collections.deque()
+        dq = collections.deque() # stores indices, values kept in decreasing order
         results = []
         for index, value in enumerate(nums):
-            # 
+            # pop smaller values from the right
+            # they can never be the max while a larger, newer value is still in the window
             while dq and nums[dq[-1]] < value:
                 dq.pop()
             dq.append(index)
 
-            if dq[0] <= index-k:
+            # drop the leftmost index once it's outside the window
+            if dq[0] <= index - k:
                 dq.popleft()
             
-            if index >= k-1:
+            # once the first window is complete, the left of the deque is always the current_max
+            if index >= k - 1:
                 results.append(nums[dq[0]])
         return results
+        # Time: O(N) - monotonic deque
+        # Tradeoff: Space - in worst case, the deque can grow up to the window size - O(K)
         '''
         
         if not nums:
