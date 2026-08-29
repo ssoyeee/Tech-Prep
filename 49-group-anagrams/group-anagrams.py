3e5a483641a111bd66fff5806e4f38e49e59f1bc
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-08-28 21:39:11 -0400

    Time: 23 ms (46.22%) | Memory: 16.2 MB (53.99%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,29 +1,11 @@
-from collections import defaultdict
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        '''
-        res = defaultdict(list) #mapping charCount to list of Anagrams
-        for s in strs:
-            count = [0] * 26 # one for each character (a~z) #1 e 1 a 1 t
-            for c in s:  #go through every single character in each string
-                #wanna know the count of how many of each characters
-                count[ord(c)-ord("a")] += 1 #increment this by 1
-            res[tuple(count)].append(s) # tuple: list is not hashable in python, it cannot be used as a dict key
-            #wanna group all anagrams to this particular count together
-        return res.values() #return the group of anagrams
-        '''
-        anagrams = defaultdict(list)      # Default value is an empty list
-        
-        for word in strs:                  
-            anagrams[''.join(sorted(word))].append(word)      
-                                       
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        anagrams = defaultdict(list)
+
+        for word in strs:
+            anagrams[''.join(sorted(word))].append(word)
         return list(anagrams.values())
-
-        # dict.values() returns a view object: 
-        # When you use my_dict.values() in Python, 
-        # it returns a "dict_values" object which is a view of the dictionary's values, not a standard list. 
-        # While you can iterate through it, 
-        # you cannot directly access elements using indexing like you would with a list. 
-
-        # Time: O(NK log K) where N is length of strs, and K is max length of string in strs, and sort each string in O(K log K)
-        # Space: O(NK) for anagrams
