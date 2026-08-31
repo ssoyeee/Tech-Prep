8ec00cf9fa4ba3399ade6a54d78dd7a77a17d1d9
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-08-31 00:28:37 -0400

    Time: 9 ms (79.23%) | Memory: 14.2 MB (46.23%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,7 +1,29 @@
-class Solution:
-    def sortedSquares(self, nums: List[int]) -> List[int]:
-        squared_result = [x**2 for x in nums]
-        return sorted(squared_result)
-        
-        # Time: O(n log n) -- uses sorted()
-        # --> with Two Pointer, O(n)
+
+class Solution(object):
+    def sortedSquares(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        # res = []
+        # for i in range(0,len(nums)):
+        #     res.append(pow(nums[i],2))
+        # return sorted(res)     
+        n = len(nums)
+        left, right = 0, n-1
+        result = [0] * n
+        pos = n - 1
+
+        while left<right:
+            if nums[left]**2 > nums[right]**2:
+                result[pos] = nums[left]**2
+                left += 1
+            else:
+                result[pos] = nums[right]**2
+                right -= 1
+            pos -= 1
+        # left == right
+        result[pos] = nums[left]**2
+        return result
+        # two pointer O(n)
+        # Time: O(n)
