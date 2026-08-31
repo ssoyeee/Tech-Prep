28d4a5ce867681e7de79e911883dd7e62a5a809c
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-08-31 13:39:00 -0400

    Time: 44 ms (24.76%) | Memory: 18.2 MB (5.34%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,13 +1,18 @@
-class Solution:
-    def subarraySum(self, nums: List[int], k: int) -> int:
+class Solution(object):
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        # prefix sum: 각 위치까지의 누적합
         count = 0
-        sums = 0
-        d = dict()
-        d[0] = 1
+        curr_sum = 0
+        prefix_counts = defaultdict(int)
+        prefix_counts[0] = 1
         
-        for i in range(len(nums)):
-            sums += nums[i]
-            count += d.get(sums-k,0)
-            d[sums] = d.get(sums,0) + 1
-        
-        return(count)    
+        for num in nums:
+            curr_sum += num
+            count += prefix_counts[curr_sum - k]
+            prefix_counts[curr_sum] += 1
+        return count
