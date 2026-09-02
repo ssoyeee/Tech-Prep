1cd388e332756b5691d618ba9051a8558145b4ea
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-09-02 10:47:16 -0400

    Time: 11 ms (37.58%) | Memory: 16.3 MB (7.22%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,28 +1,31 @@
-class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        if not prerequisites:
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        graph = defaultdict(list)
+        for course, prereq in prerequisites:
+            graph[course].append(prereq)
+        visiting = set()
+        visited = set()
+
+        def dfs(node):
+            if node in visiting:
+                return False
+            if node in visited:
+                return True
+            visiting.add(node)
+
+            for prereq in graph[node]:
+                if not dfs(prereq):
+                    return False
+            visiting.remove(node)
+            visited.add(node)
             return True
         
-        graph = collections.defaultdict(list)
-        indegrees = [0] * numCourses
-        
-        for prerequisite in prerequisites:
-            graph[prerequisite[1]].append(prerequisite[0])
-            indegrees[prerequisite[0]] += 1
-
-        queue = deque(i for i in range(numCourses) if indegrees[i] == 0)      
-        completedCourses = 0    
-        while queue:
-            node = queue.popleft()
-            for dependent_node in graph[node]:
-                indegrees[dependent_node] -= 1
-                if indegrees[dependent_node] == 0:
-                    queue.append(dependent_node)
-            
-            completedCourses += 1
-
-        return numCourses == completedCourses
-
-    # Topological Sort by BFS
-    # Time: O(V+E)
-    # Space: O(V+E)
+        for course in range(numCourses):
+            if not dfs(course):
+                return False
+        return True
