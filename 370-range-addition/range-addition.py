0492eda3df90d02198d46d3b9082690fe363c51b
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-09-08 11:37:03 -0400

    Time: 3 ms (93.50%) | Memory: 24.3 MB (85.92%) - LeetSync

---
+++ 370-range-addition/range-addition.py
@@ -0,0 +1,16 @@
+class Solution:
+    def getModifiedArray(self, length: int, updates: List[List[int]]) -> List[int]:
+        # difference array
+        diff = [0] * length
+        for start, end, inc in updates:
+            diff[start] += inc
+            if end+1<length:
+                diff[end+1] -= inc
+
+        arr = [0] * length
+        arr[0] = diff[0]
+        for i in range(1, length):
+            arr[i] = arr[i-1]+diff[i]
+        return arr
+        # brute-force: Time O(m * n)
+        # difference array + prefix
