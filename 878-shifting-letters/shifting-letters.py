746b6983aed9905c13d5ecac8c8d6fbf3af13be7
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-09-08 13:45:07 -0400

    Time: 91 ms (50.72%) | Memory: 31.3 MB (50.18%) - LeetSync

--- 878-shifting-letters/shifting-letters.py
+++ 878-shifting-letters/shifting-letters.py
@@ -9,3 +9,6 @@ class Solution:
             new_index = ((ord(s[i]) - ord('a'))+shifts[i]) % 26
             result.append(chr(new_index + ord('a')))
         return ''.join(result)
+
+        # Time: O(n) -- where n is len(s) and m is len(shifts), and length of s and shifts are the same, so O(n) + O(n) = O(2n) = O(n)
+        # Space: O(n) -- result array
