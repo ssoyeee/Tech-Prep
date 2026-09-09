2dccf98203ff12ede89093336c493a807153f271
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-09-09 13:48:50 -0400

    Time: 59 ms (10.05%) | Memory: 17.7 MB (93.66%) - LeetSync

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -1,10 +1,22 @@
-class Solution:
-    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        total, tank, start = 0, 0, 0
+class Solution(object):
+    def canCompleteCircuit(self, gas, cost):
+        """
+        :type gas: List[int]
+        :type cost: List[int]
+        :rtype: int
+        """
+        total_tank, current_tank, start_index = 0, 0, 0
+
         for i in range(len(gas)):
-            tank += gas[i] - cost[i]
-            total += gas[i] - cost[i]
-            if tank < 0:
-                start = i +1
-                tank = 0
-        return start if total >=0 else -1
+            total_tank += gas[i]
+            total_tank -= cost[i]
+            current_tank += gas[i]
+            current_tank -= cost[i]
+
+            if current_tank < 0:
+                start_index = i+1
+                current_tank = 0
+        if total_tank < 0:
+            return -1
+        else: 
+            return start_index
