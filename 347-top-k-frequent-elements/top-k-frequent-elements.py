6f1a6f85fcd4463567dc0b3a79647a53d09d92d0
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-09-09 18:39:33 -0400

    Time: 2 ms (92.18%) | Memory: 23 MB (42.05%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -39,3 +39,8 @@ class Solution:
                 if len(res) == k:
                     return res
         '''
+        '''
+        counted_nums = Counter(nums)
+        sorted_counts = sorted(counted_nums.items(), key=lambda x: (-x[1], x[0]))
+        return [val for val, freq in sorted_counts[:k]]
+        '''
