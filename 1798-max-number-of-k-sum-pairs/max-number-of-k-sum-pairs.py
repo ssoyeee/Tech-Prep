59006942c91a3759d56dd12442e1ffe9c8cbe8a4
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-09-12 23:47:53 -0400

    Time: 580 ms (12.43%) | Memory: 21.9 MB (69.11%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
@@ -1,33 +1,26 @@
-class Solution:
-    def maxOperations(self, nums: List[int], k: int) -> int:
-        # finding two nums(a+b) that could equals to k when a+b
-        # two pointers
+class Solution(object):
+    def maxOperations(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
         nums.sort()
-        l, r = 0, len(nums)-1
         op = 0
-
-        while l < r:
-            if (nums[l] + nums[r]) == k:
-                l += 1
-                r -= 1
+        left = 0
+        right = len(nums)-1
+        for i in range(len(nums)):
+            if nums[i] == nums[i-1]:
+                continue
+        while left < right:
+            if nums[left]+nums[right] < k:
+                left += 1
+            elif nums[left]+nums[right] > k:
+                right -= 1
+            else:
                 op += 1
+                left += 1
+                right -= 1
 
-            elif (nums[l] + nums[r]) < k: 
-                l += 1
-            else: # >k
-                r -= 1
+
         return op
-        '''
-
-        nums = [1,2,3,4]
-        k = 5
-        l = 1
-        r = 4
-        l+r = 5
-        [2, 3]
-        l = 2
-        r = 3
-            = 5
-
-        op = 2
-        '''
