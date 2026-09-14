3c996f12f32f2144967ef232be1d9a17d77a2afb
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-09-13 23:19:35 -0400

    Time: 12 ms (50.94%) | Memory: 25.8 MB (97.00%) - LeetSync

--- 359-logger-rate-limiter/logger-rate-limiter.py
+++ 359-logger-rate-limiter/logger-rate-limiter.py
@@ -1,17 +1,21 @@
 class Logger:
 
     def __init__(self):
-        self.last_seen = {}
+        self.last_seen = {} # {"foo": 11}
 
     def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
         if message not in self.last_seen:
+            # first time seeing this message
             self.last_seen[message] = timestamp + 10
             return True
         else:
             if timestamp >= self.last_seen[message]:
+                # if 10 sec have passed, allow again
                 self.last_seen[message] = timestamp + 10
                 return True
             return False
+    # Time: O(1) per call -- dict lookup and update O(1)
+    # Space: O(m) -- where n is number of uniq messages ever seen
 
 
 # Your Logger object will be instantiated and called as such:
