f605ace151e47b16cf6456c1b26d27dce696f77c
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-09-16 00:18:06 -0400

    Time: 16 ms (57.19%) | Memory: 14.2 MB (16.99%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,28 +1,24 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        strs = []
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if not s:
+            return True
+
+        sentence = []
         for c in s:
-            if c.isalnum(): #alphabet? of numeric?
-                strs.append(c.lower()) 
-        while len(strs) > 1:
-            # letters in list are at least 2
-            if strs.pop(0) != strs.pop(): 
-                # if the poped first letter is different with the last poped letter
-                # O(N) => because we need to arrange them again + O(1)
-                return False
+            if c.isalnum():
+                sentence.append(c.lower())
+
+        left = 0
+        right = len(sentence)-1
         
-        return True
-
-
-# from collections import deque
-
-# class Solution:
-#     def isPalindrome(self, s:str) -> bool:
-#     strs = deque()
-#     for c in s:
-#         if c.isalnum():
-#             strs.append(c.lower())
-#     while len(strs) >1:
-#         if strs.popleft() != strs.pop(): #O(1)+O(1)
-#             return False
-#     return True
+        while left<right:
+            if sentence[left] == sentence[right]:
+                left += 1
+                right -= 1
+            else:
+                return False            
+        return True    
