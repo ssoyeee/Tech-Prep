ee8ca5f4536cde7e2993feb14576db24b6b167f3
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-09-21 00:40:27 -0400

    Time: 179 ms (41.20%) | Memory: 12.7 MB (15.74%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.py
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.py
@@ -5,7 +5,7 @@ class Solution(object):
         :type right: int
         :rtype: int
         """
-        prime_set = [2, 3, 5, 7, 11, 13, 17, 19]
+        prime_set = {2, 3, 5, 7, 11, 13, 17, 19}
         count = 0
 
         for val in range(left, right+1):
