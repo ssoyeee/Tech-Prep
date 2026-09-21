df918633010d6d4fdef33ec9eb47a163144f0b66
Soyeon <111319810+ssoyeee@users.noreply.github.com>
2026-09-21 00:33:30 -0400

    Time: 163 ms (37.49%) | Memory: 19.5 MB (8.52%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.py
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.py
@@ -2,21 +2,24 @@ from collections import defaultdict
 class Solution:
     def countPrimeSetBits(self, left: int, right: int) -> int:
         count = 0
-        bi_nums = []
+        bit_counts = []
 
-        for i in range(left, right+1):
-            binary_clean = format(i, 'b').count('1')
-            bi_nums.append(binary_clean)
+        for num in range(left, right + 1):
+            ones = format(num, 'b').count('1')
+            bit_counts.append(ones)
 
-        for n in bi_nums:
-            if n <= 1:
+        for val in bit_counts:
+            if val <= 1:
                 continue
-            limit = int(math.sqrt(n))
 
-            for c in range(2, limit+1):
-                if n % c == 0:
+            limit = int(math.sqrt(val))
+
+            for div in range(2, limit + 1):
+                if val % div == 0:
                     break
             else:
                 count += 1
 
         return count
+        # Time: O(n * sqrt(B)) -- where B is max set bits, n is right - left+1
+        # Space: O(n)
